831a9e07d917e973e85172a149cb29d6880553d3
Darío Vargas <dariovm13@gmail.com>
2020-11-29 22:22:30 -0600

    [NEW]: Insersión de catalogos mediante XML

---
+++ Scripts/Script catalogos.sql
@@ -0,0 +1,67 @@
+USE ProyectoBD1
+--Se lee el archivo XML
+DECLARE @xmlData XML
+
+SET @xmlData = (
+		SELECT *
+		FROM OPENROWSET(BULK 'C:\Users\dvarg\Desktop\TEC\2020\Segundo Semestre\Bases de datos\Proyectos\Proyecto 2\BDTarea2\XML\catalogos.xml', SINGLE_BLOB) AS xmlData
+		)
+
+--Inserta el tipo de documento de indentidad de los xml
+INSERT INTO TipoDocIdentidad (id, Nombre )
+SELECT	ref.value('@Id', 'int')
+		,ref.value('@Nombre', 'VARCHAR(100)')
+FROM @xmlData.nodes('Catalogos/Tipo_Doc/TipoDocuIdentidad') xmlData(ref)
+LEFT JOIN TipoDocIdentidad TD on TD.id =  ref.value('@Id', 'int')
+WHERE TD.id IS NULL
+
+--Inserta los tipos de moneda
+INSERT INTO TipoMoneda (id, Nombre, Simbolo)
+SELECT	ref.value('@Id', 'int')
+		,ref.value('@Nombre', 'VARCHAR(100)')
+		,ref.value('@Simbolo', 'char(10)')
+FROM @xmlData.nodes('Catalogos/Tipo_Moneda/TipoMoneda') xmlData(ref)
+LEFT JOIN TipoMoneda TM on TM.id =  ref.value('@Id', 'int')
+WHERE TM.id IS NULL
+
+--Inserta los Catalogos del parentezco
+INSERT INTO Parentezco (ID, NOMBRE)
+SELECT	ref.value('@Id', 'int')
+		,ref.value('@Nombre', 'VARCHAR(100)')
+FROM @xmlData.nodes('Catalogos/Parentezcos/Parentezco') xmlData(ref)
+LEFT JOIN Parentezco P on P.id =  ref.value('@Id', 'int')
+WHERE P.id IS NULL
+
+--Inserta los datos de tipo de cuenta ahorro
+INSERT INTO TipoCuentaAhorro (
+	id
+	,Nombre
+	,IdTipoMoneda
+	,SaldoMinimo
+	,MultaSaldoMin
+	,CargoAnual
+	,NumRetirosHumano
+	,NumRetirosAutomatico
+	,ComisionHumano
+	,ComisionAutomatico
+	,Interes
+	)
+SELECT	ref.value('@Id', 'int')
+		,ref.value('@Nombre', 'varchar(100)')
+		,ref.value('@IdTipoMoneda', 'int')
+		,ref.value('@SaldoMinimo', 'money')
+		,ref.value('@MultaSaldoMin', 'float')
+		,ref.value('@CargoMensual', 'float')
+		,ref.value('@NumRetirosHumano', 'int')
+		,ref.value('@NumRetirosAutomatico', 'int')
+		,ref.value('@ComisionHumano', 'int')
+		,ref.value('@ComisionAutomatico', 'int')
+		,ref.value('@Interes', 'int')
+FROM @xmlData.nodes('Catalogos/Tipo_Cuenta_Ahorros/TipoCuentaAhorro') xmlData(ref)
+LEFT JOIN TipoCuentaAhorro TC on TC.id =  ref.value('@Id', 'int')
+WHERE TC.id IS NULL
+
+Select * from TipoDocIdentidad
+Select * from TipoMoneda
+Select * from Parentezco
+Select * from TipoCuentaAhorro
