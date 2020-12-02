0a2138b5f09959b1b4d1cac6c43477fb77f7f58a
Darío Vargas <dariovm13@gmail.com>
2020-12-02 10:41:19 -0600

    [CONTINUE]: SP con procesar movimientos y cerrar estados de cuenta

---
+++ Stored Procedures/CerrarEstadoCuenta.sql
@@ -0,0 +1,95 @@
+USE [ProyectoBD1]
+GO
+/****** Object:  StoredProcedure [dbo].[InsertarMovimientos]    Script Date: 12/1/2020 2:43:00 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[CerrarEstadoCuenta] 
+	@inCuentaCierra INT
+	,@inFechaInicio DATE
+	,@inFechaFin DATE
+	,@OutMovimientoId INT OUTPUT
+	,@OutResultCode INT OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	BEGIN TRY
+		-- Se declaran variables
+		DECLARE @SaldoFinal MONEY = 0
+		DECLARE @TipoCuentaAhorroId INT
+		DECLARE @CuentaId INT, @EstadoCuentaId INT
+
+		-- Se inicializan variables
+		SELECT @OutResultCode = 0
+		SELECT @CuentaId = Id FROM CuentaAhorro WHERE NumeroCuenta = @inCuentaCierra
+		SELECT @TipoCuentaAhorroId = TipoCuentaId FROM CuentaAhorro CA WHERE CA.id = @CuentaId
+		SELECT @EstadoCuentaId = id FROM EstadoCuenta EC WHERE EC.CuentaAhorroid = @CuentaId AND EC.FechaInicio = @inFechaInicio
+
+		SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+		BEGIN TRANSACTION TSaveCerrEst
+
+			DECLARE @EstadoDeCuentaID INT
+					,@OutNumRetirosHumano INT
+					,@OutNumRetirosCajero INT
+					,@OutSaldoFinal MONEY
+					,@OutMovimientoECId INT
+					,@OutResultCodeEC INT
+
+			EXEC [dbo].[ProcesarMovimientos] 
+					@inCuentaCierra
+					,@inFechaInicio
+					,@inFechaFin
+					,@OutNumRetirosHumano OUTPUT
+					,@OutNumRetirosCajero OUTPUT
+					,@OutSaldoFinal OUTPUT
+					,@OutMovimientoECId OUTPUT
+					,@OutResultCodeEC OUTPUT
+
+			SELECT  @SaldoFinal = @OutSaldoFinal
+
+			IF(@OutNumRetirosHumano > (SELECT NumRetirosHumano FROM TipoCuentaAhorro TC WHERE TC.id = @TipoCuentaAhorroId) )
+				BEGIN
+					SELECT @SaldoFinal = @SaldoFinal - (SELECT ComisionHumano FROM TipoCuentaAhorro TC WHERE TC.id = @TipoCuentaAhorroId)
+				END;
+
+			IF(@OutNumRetirosCajero > (SELECT NumRetirosAutomatico FROM TipoCuentaAhorro TC WHERE TC.id = @TipoCuentaAhorroId) )
+				BEGIN
+					SELECT @SaldoFinal = @SaldoFinal - (SELECT ComisionAutomatico FROM TipoCuentaAhorro TC WHERE TC.id = @TipoCuentaAhorroId)
+				END;
+			--CALCULAR INTERESES
+			--CALCULAR MULTAS POR SALDO MINIMO
+
+			--Se actualiza el movimiento en la base de datos
+			UPDATE dbo.EstadoCuenta
+			SET SaldoFinal = @SaldoFinal
+			WHERE Id = @EstadoCuentaId
+
+			SET @outMovimientoId = SCOPE_IDENTITY();
+
+		COMMIT TRANSACTION TSaveCerrEst;
+	END TRY
+
+	BEGIN CATCH
+		IF @@TRANCOUNT > 0 
+			ROLLBACK TRANSACTION TSaveCerrEst;
+
+		INSERT INTO dbo.Errores
+		VALUES (
+			SUSER_SNAME()
+			,ERROR_NUMBER()
+			,ERROR_STATE()
+			,ERROR_SEVERITY()
+			,ERROR_LINE()
+			,ERROR_PROCEDURE()
+			,ERROR_MESSAGE()
+			,GETDATE()
+			);
+
+		SET @OutResultCode = 50005;
+	END CATCH
+
+	SET NOCOUNT OFF
+END;
---
+++ Stored Procedures/ProcesarMovimientos.sql
@@ -0,0 +1,98 @@
+USE [ProyectoBD1]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[ProcesarMovimientos] 
+	@inCuentaId INT
+	,@inFechaInicio DATE
+	,@inFechaFin DATE
+	,@OutNumRetirosHumano INT OUTPUT
+	,@OutNumRetirosCajero INT OUTPUT
+	,@OutSaldoFinal MONEY OUTPUT
+	,@OutMovimientoId INT OUTPUT
+	,@OutResultCode INT OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	BEGIN TRY
+		-- se declaran variables
+		DECLARE @nuevoSaldo MONEY = 0
+		DECLARE @Movimientos TABLE(Sec INT IDENTITY(1,1),MovimientoId INT, Tipo VARCHAR (20), Nombre VARCHAR (20), Monto MONEY)	--Tabla con los movimientos del mes
+		DECLARE @MovimientoId INT
+		DECLARE @CuentaId INT
+
+		--Se Inicializa la tabla con los movimientos
+		SELECT @CuentaId = Id FROM CuentaAhorro WHERE NumeroCuenta = @inCuentaId
+
+		INSERT INTO @Movimientos(MovimientoId, Tipo, Nombre, Monto)
+		SELECT	MC.id
+				,TM.TipoOperacion
+				,TM.Nombre
+				,MC.Monto
+		FROM MovimientoCuentaAhorro MC
+		INNER JOIN TipoMovimientoCuentaAhorro TM ON TM.id = MC.TipoMovimientoCuentaAhorroid
+		WHERE  MC.CuentaAhorroid = @CuentaId
+
+		-- se inicializan variables
+		SELECT @OutResultCode = 0
+		SELECT @OutNumRetirosCajero = COUNT(M.Nombre) FROM @Movimientos M Where M.Nombre = 'Retiro ATM'
+		SELECT @OutNumRetirosHumano = COUNT(M.Nombre) FROM @Movimientos M Where M.Nombre = 'Retiro Ventana'
+
+		SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+		BEGIN TRANSACTION TSaveProcMov
+
+			DECLARE @lo int = 1, @hi int
+			SELECT @hi = max(sec) FROM @Movimientos
+			--INICIO WHILE 
+			WHILE @lo <= @hi
+				BEGIN 
+					SELECT @MovimientoId  = M.MovimientoId FROM @Movimientos M WHERE M.sec = @lo	--Id del movimiento que se esta iterando
+					IF (SELECT Tipo From @Movimientos M Where M.Sec = @lo) = 'Credito'				--Si el tipo de movimiento actual es un credito
+						BEGIN
+							SELECT @nuevoSaldo = @nuevoSaldo + M.Monto FROM @Movimientos M WHERE M.sec = @lo	--Se suma el credito al nuevo saldo
+						END;
+					ELSE
+						BEGIN
+							SELECT @nuevoSaldo = @nuevoSaldo - M.Monto FROM @Movimientos M WHERE M.sec = @lo	--Se resta el debito al nuevo saldo
+						END;
+
+					--Se actualiza el movimiento en la base de datos
+					UPDATE dbo.MovimientoCuentaAhorro
+					SET NuevoSaldo = @nuevoSaldo
+					WHERE Id = @MovimientoId
+					SELECT @MovimientoId
+					SELECT NuevoSaldo FROM MovimientoCuentaAhorro WHERE Id = @MovimientoId
+					SET @lo = @lo + 1
+				END
+			--FIN WHILE
+
+			SET @OutSaldoFinal = @nuevoSaldo
+			SET @outMovimientoId = SCOPE_IDENTITY()
+		COMMIT TRANSACTION TSaveProcMov;
+	END TRY
+
+	BEGIN CATCH
+		IF @@TRANCOUNT > 0 
+			ROLLBACK TRANSACTION TSaveProcMov;
+
+		INSERT INTO dbo.Errores
+		VALUES (
+			SUSER_SNAME()
+			,ERROR_NUMBER()
+			,ERROR_STATE()
+			,ERROR_SEVERITY()
+			,ERROR_LINE()
+			,ERROR_PROCEDURE()
+			,ERROR_MESSAGE()
+			,GETDATE()
+			);
+
+		SET @OutResultCode = 50005;
+	END CATCH
+
+	SET NOCOUNT OFF
+END;
