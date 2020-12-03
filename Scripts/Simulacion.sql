3b38d6a43f98ca36a44acfadcd4f053d730984aa
Darío Vargas <dariovm13@gmail.com>
2020-12-02 19:07:32 -0600

    [FIX]: Insersion de usuarios y usuarios puede ver

--- Scripts/Simulacion.sql
+++ Scripts/Simulacion.sql
@@ -4,7 +4,7 @@ DECLARE @xmlData XML
 -- Nota: cambiar el path del from C:\Users\yeico\Desktop\BDTarea2\XML\catalogos.xml
 SET @xmlData = (
 		SELECT *
-		FROM OPENROWSET(BULK 'C:\Users\dvarg\Desktop\TEC\2020\Segundo Semestre\Bases de datos\Proyectos\Proyecto 2\BDTarea2\XML\operaciones.xml', SINGLE_BLOB) 
+		FROM OPENROWSET(BULK 'C:\Users\dvarg\Desktop\TEC\2020\Segundo Semestre\Bases de datos\Proyectos\Proyecto 2\BDTarea2\XML\Datos_Tarea_2.xml', SINGLE_BLOB) 
 		AS xmlData
 		)
 
@@ -24,15 +24,41 @@ WHILE @lo <= @hi
 BEGIN
 	SELECT @FechaOperacion=F.Fecha FROM @FechasProcesar F WHERE F.sec=@lo	
 	SELECT @TablaFecha = xmlData.ref.query('.') FROM @xmlData.nodes('Operaciones/FechaOperacion') AS xmlData(ref) WHERE ref.value('@Fecha','date') = @FechaOperacion --SE GUARDA EN @TablaFecha LA TABLA ACTUAL A PROCESAR.
+--SE PROCESA LA  INSERCION DE USUARIOS
+	INSERT INTO Usuario(
+		NombreUsuario
+		,Pass
+		,ValorDocIdentidad
+		,EsAdmi
+	)
+	SELECT	ref.value('@User', 'varchar(50)')
+			,ref.value('@Pass', 'varchar(50)')
+			,ref.value('@ValorDocumentoIdentidad', 'int')
+			,ref.value('@EsAdministrador', 'bit')	
+	FROM @TablaFecha.nodes('FechaOperacion/Usuario') AS xmlData(ref) 
+	LEFT JOIN Usuario U on U.ValorDocIdentidad = ref.value('@ValorDocumentoIdentidad', 'int')
+	WHERE U.id IS NULL
+--SE PROCESA LA  INSERCION DE USUARIOS PUEDE VER
+	INSERT INTO UsuarioPuedeVer(
+		NombreUsuario
+		,NumeroCuenta
+		,Usuarioid
+	)
+	SELECT	ref.value('@User', 'varchar(100)')
+			,ref.value('@NumeroCuenta', 'varchar(100)')
+			,U.id
+	FROM @TablaFecha.nodes('FechaOperacion/UsuarioPuedeVer') AS xmlData(ref) 
+	INNER JOIN Usuario U ON U.NombreUsuario = ref.value('@User', 'varchar(100)')
 
 --SE PROCESA LA  INSERCION DE CLIENTES EN LA FECHA DE OPERACION IGUAL A @FechaOperacion
-	INSERT INTO Persona(Nombre
-			,ValorDocIdentidad
-			,Email
-			,FechaNacimiento
-			,Telefono1
-			,Telefono2
-			,TipoDocIdentidadid	
+	INSERT INTO Persona(
+		Nombre
+		,ValorDocIdentidad
+		,Email
+		,FechaNacimiento
+		,Telefono1
+		,Telefono2
+		,TipoDocIdentidadid	
 	)
 	SELECT	ref.value('@Nombre', 'varchar(100)')
 			,ref.value('@ValorDocumentoIdentidad', 'int')
@@ -52,7 +78,7 @@ BEGIN
 		,NumeroCuenta
 		,FechaCreacion
 		,Saldo
-		)
+	)
 	SELECT	P.id
 			,ref.value('@TipoCuentaId', 'int')
 			,ref.value('@NumeroCuenta', 'int')
@@ -73,7 +99,7 @@ BEGIN
 		,Porcentaje
 		,Activo
 		,FechaDesactivacion
-		)
+	)
 	SELECT	P.id
 			,C.id
 			,C.NumeroCuenta
@@ -89,32 +115,37 @@ BEGIN
 	WHERE B.NumeroCuenta IS NULL
 
 --ESTADOS DE CUENTA DE LA FECHA DE OPERACION
-	DECLARE @CuentasCierran TABLE(sec int identity(1,1), codigocuenta varchar (20))
-	DECLARE @lo1 int = 1, @hi1 int
+	--Se declaran las variables
+	DECLARE @CuentasCierran TABLE(sec int identity(1,1), codigocuenta varchar (20))	--Tabla con las cuentas que se cierran
+	DECLARE @lo1 int, @hi1 int							
 
+	--Se le asignan valores
+	SELECT @lo1 = min(Sec), @hi1 = max(Sec) from @CuentasCierran
+
+	--Se insertan valores en la tabla de @CuentasCierran
 	INSERT @CuentasCierran(codigocuenta)
 	SELECT ref.value('@NumeroCuenta','int') 
-	FROM @TablaFecha.nodes('FechaOperacion/Cuenta') AS xmlData(ref) 
+	FROM @TablaFecha.nodes('FechaOperacion/Cuenta') AS xmlData(ref)
 
 --SE CREAN LOS ESTADOS DE CUENTA
-		INSERT INTO EstadoCuenta(
+	INSERT INTO EstadoCuenta(
 			CuentaAhorroid
 			,NumeroCuenta
 			,FechaInicio
 			,FechaFin
 			,SaldoInicial
 			,SaldoFinal
-			)
-		SELECT	C.id
-				,ref.value('@NumeroCuenta', 'int')
-				,@fechaOperacion
-				,DATEADD(month, 1, @fechaOperacion)
-				,C.Saldo
-				,C.Saldo
-		FROM @TablaFecha.nodes('FechaOperacion/Cuenta') AS xmlData(ref) 
-		INNER JOIN CuentaAhorro C ON C.NumeroCuenta = ref.value('@NumeroCuenta', 'int')
-		LEFT JOIN EstadoCuenta EC ON EC.FechaInicio = @fechaOperacion
-		WHERE EC.FechaInicio IS NULL
+	)
+	SELECT	C.id
+			,ref.value('@NumeroCuenta', 'int')
+			,@fechaOperacion
+			,DATEADD(month, 1, @fechaOperacion)
+			,C.Saldo
+			,C.Saldo
+	FROM @TablaFecha.nodes('FechaOperacion/Cuenta') AS xmlData(ref) 
+	INNER JOIN CuentaAhorro C ON C.NumeroCuenta = ref.value('@NumeroCuenta', 'int')
+	LEFT JOIN EstadoCuenta EC ON EC.FechaInicio = @fechaOperacion
+	WHERE EC.FechaInicio IS NULL
 
 --SE PROCESAN LOS MOVIMIENTOS DE LA CUENTA
 	INSERT INTO MovimientoCuentaAhorro(
@@ -138,23 +169,41 @@ BEGIN
 	INNER JOIN CuentaAhorro CA ON CA.NumeroCuenta = ref.value('@CodigoCuenta', 'int')
 
 --INICIO WHILE ESTADO CUENTA
-	--SELECT @lo1=min(sec), @hi1=max(sec) FROM @CuentasCierran
-	--WHILE @lo1 <= @hi1
-	--	BEGIN 
-	--		SELECT @CuentaCierra=C.codigocuenta FROM @CuentasCierran C WHERE C.sec=@lo1
+	SELECT @lo1=min(sec), @hi1=max(sec) FROM @CuentasCierran
+	WHILE @lo1 <= @hi1
+		BEGIN 
+			SELECT @CuentaCierra  = C.codigocuenta FROM @CuentasCierran C WHERE C.sec = @lo1
+			
+			DECLARE	@OutMovimientoId INT
+					,@OutResultCode INT
+					,@FechaFin DATE
+			SELECT @FechaFin = DATEADD(month, 1, @fechaOperacion)
+			EXEC [dbo].[CerrarEstadoCuenta] 
+				@CuentaCierra
+				,@fechaOperacion
+				,@FechaFin
+				,@OutMovimientoId OUTPUT
+				,@OutResultCode OUTPUT
 
-		--	SET @LO1 = @LO1 + 1
-		--END
+			SET @LO1 = @LO1 + 1
+		END
 --FIN WHILE 
 
 	DELETE @CuentasCierran
 	SET @lo = @lo + 1
 END;
+--SELECT * FROM Usuario
+--SELECT * FROM UsuarioPuedeVer
 --SELECT * FROM Persona
 --SELECT * FROM CuentaAhorro
 --SELECT * FROM Beneficiarios
-SELECT * FROM EstadoCuenta
-SELECT * FROM MovimientoCuentaAhorro
---Delete FROM Beneficiarios
---DELETE MovimientoCuentaAhorro
+--SELECT * FROM EstadoCuenta
+--SELECT * FROM MovimientoCuentaAhorro
+
+--DELETE Usuario
+--DELETE UsuarioPuedeVer
+--DELETE Persona
+--DELETE CuentaAhorro
+--DELETE Beneficiarios
 --DELETE EstadoCuenta
+--DELETE MovimientoCuentaAhorro
